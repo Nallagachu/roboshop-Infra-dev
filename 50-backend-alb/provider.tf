0bd97ff55a1fcf28d7b8278c95ccd014782a1e3f
durgababu <durgababu.nallagachu@gmail.com>
2025-07-01 11:51:24 +0530

    updated url

--- 50-backend-alb/provider.tf
+++ 50-backend-alb/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "84s-remote-state-dev"
+    bucket = "roboshop-project-durgababu"
     key    = "roboshop-dev-backend-alb"
     region = "us-east-1"
     encrypt        = true
